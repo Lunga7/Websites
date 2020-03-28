05e8713450cb959fe6be6db13c56baeff78b4c3f
Lunga7 <lungangonyama540@gmail.com>
2020-03-28 12:15:15 +0200

    modified about and userHome pages

--- resources/views/userhome.blade.php
+++ resources/views/userhome.blade.php
@@ -4,7 +4,7 @@
  <div class="row align-items-center justify-content-center">
           <div class="col-md-7 text-center" data-aos="fade-up" data-aos-delay="400">
             <h1 class="mb-4">The Art platform</h1>
-            <p><a href="#" class="btn btn-primary px-4 py-3">Download</a>   <a href="#" class="btn btn-primary px-4 py-3">Upload</a></p>
+            <p><a href="#" class="btn btn-primary px-4 py-3">Download</a>   <a href="#" class="btn btn-primary px-4 py-3">Upload Art</a></p>
           
           </div>
         </div>
