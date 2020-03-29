aa456d2c1dde44437153d24e3be5d5b0bb110cff
Lunga7 <lungangonyama540@gmail.com>
2020-03-29 15:07:07 +0200

    making more changes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+
 Route::get('/userhome', 'PagesController@getUserHome');
 
 Route::get('/about', 'PagesController@getAbout');
@@ -32,9 +33,6 @@ Route::get('/upload', function () {
 });
 
 
-
-
-Route::get('/home','HomeController@index')->name('home');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'HomeController@index')->name('home');
